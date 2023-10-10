010002699457eab70ed7b9604cda28ae34570888
sidra mursaleen <sidramursaleen333@gmail.com>
2023-10-10 12:31:54 +0500

    EVENT(JS) ASSIGNMENT

---
+++ script.js
@@ -0,0 +1,111 @@
+// EVENT
+
+// part of question no 2
+
+// function visible (){
+//     document.getElementById("box2").style.visibility = "hidden";
+// }
+
+// part of question no 12
+
+// var newWindow;
+
+// function closeWindow() {
+//   newWindow.close();
+// }
+
+// function openWindow() {
+//   newWindow = window.open("", "", "width=600px,height=400px");
+//   newWindow.document.write("<button id='close'>Click here</button>");
+//   newWindow.document
+//     .getElementById("close")
+//     .addEventListener("click", closeWindow);
+// }
+
+// part of question no 5
+
+// var myAudio = document.getElementById("audio");
+
+// function playAudio(){
+//     myAudio.play();
+// }
+
+// READING AND SETTING FIELD VALUES
+
+// part of question no 1
+
+// function getName() {
+//     var name = document.getElementById("name");
+//     console.log(name.value);
+// }
+
+// part of question no 2
+
+// var selected = [];
+
+// function getOption(e) {
+//   selected.push(e.value);
+// }
+
+// function printOption() {
+//   console.log(selected);
+// }
+
+// part of question no 3
+
+// var selected = [];
+
+// function getOption(e) {
+//   selected.push(e.value);
+// }
+
+// function printOption() {
+//   console.log(selected);
+// }
+
+// part of question no 4
+
+// function input () {
+//     userName.value = "input field"
+// }
+
+// part of question no 5
+
+// var married ;
+
+// function getValue(value) {
+//     married =  maritalstatus.value ;
+//     if(married != "married"){
+//         maritalstatus.value = "available"
+//     }
+//     else{
+//         console.log(married)
+//     }
+// }
+
+// part of question no 6
+
+// function input() {
+//     var inputName = document.getElementById("userName").value;
+//     if(inputName){
+//         console.log(userName.value)
+//     }
+//     else{
+//         userName.value = "ENTER IT"
+//     }
+// }
+
+// part of question no 7
+
+// function getValues() {
+//   var firstName = document.getElementById("firstname").value;
+//   var lastNmae = document.getElementById("lastname").value;
+//   var qualification = document.getElementById("qualification").value;
+//   var course = document.getElementById("course").value;
+
+//   if (firstName && lastNmae && qualification && course) {
+//     alert("Your form has been submitted");
+//   } else {
+//     alert("plz fill all the fields");
+//   }
+// }
